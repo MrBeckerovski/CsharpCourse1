ab5fa3aa77c51dc67ee96114f0efdb767b7e397c
Евгений Беккер <eug90@bk.ru>
2021-08-03 14:37:11 +0300

    Homework 1

---
+++ EX1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут? : ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Какая у вас фамилия? : ");
+            var family = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет? : ");
+            var age = Console.ReadLine();
+            Console.WriteLine("Какого вы роста? : ");
+            var height = Console.ReadLine();
+            Console.WriteLine("Сколько вы весите? : ");
+            var weight = Console.ReadLine();
+            var sum = name + family + age + height + weight;
+            Console.WriteLine($"{name} + {family} + {age} + {height} + {weight} = {sum}");
+        }
+    }
+}
+    
---
+++ EX3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EX3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double x1 = 0; 
+            Double x2 = 0;
+            Double y1 = 0;
+            Double y2 = 0;
+            Double distance;
+           
+            Console.Write("x1=");
+            x1 = double.Parse(Console.ReadLine());
+            Console.Write("x2=");
+            x2 = double.Parse(Console.ReadLine());
+            Console.Write("y1=");
+            y1 = double.Parse(Console.ReadLine());
+            Console.Write("y2=");
+            y2 = double.Parse(Console.ReadLine());
+
+            {
+                distance = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+                
+                Console.WriteLine("distance:" + "{0:F2}", distance);
+                Console.ReadLine();
+
+
+
+
+
+            }
+        }
+    }
+}
