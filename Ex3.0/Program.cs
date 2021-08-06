bcb677892e83ee64c7d6491f14745b6035d6acfb
Евгений Беккер <eug90@bk.ru>
2021-08-06 14:00:18 +0300

    !

---
+++ Ex3.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex3._0
+{
+    class Program
+    // С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+    // Беккер
+    {
+        static void Main(string[] args)
+        {
+                {
+                    int sum = 0;
+                    int num = 0;
+
+                    do
+                    {
+                        num = int.Parse(Console.ReadLine());
+                        if (num > 0 && num % 2 == 1)
+                            sum += num;
+
+                    } while (num != 0);
+
+                    Console.WriteLine("Sum: " + sum);
+                }
+        }
+    }
+}
